f28e6f28337b59166683183687c136c7718b0090
nanami69 <69er.11.11@gmail.com>
2023-04-06 21:50:19 +0900

    feat: create flask app

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 import sys
+from flask import Flask
+app = Flask(__name__)
 
 def sum(x, y):
   return x+y
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
     result = sum(num1, num2)
     print(result)
+
+    app.run()
