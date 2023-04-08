e1832c4a04bd6a37bdfb91bf249a19023653fc9b
nanami69 <69er.11.11@gmail.com>
2023-04-08 17:27:55 +0900

    feat: use templates

--- index.py
+++ index.py
@@ -1,22 +1,14 @@
-import sys
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def sum(x, y):
-  return x+y
-
-@app.route('/')
+@app.route('/index')
 def index():
-    return app.send_static_file('index.html')
+    name = 'nanami'
+    return render_template('index.html', name=name)
 
 @app.route('/sub')
 def sub():
     return "This is Sub Page!"
 
 if __name__ == "__main__":
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[2])
-    result = sum(num1, num2)
-    print(result)
-
     app.run()
