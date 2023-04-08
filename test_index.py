b48bb27e8e147fc96d70ba5fbf92110cf42133ed
nanami69 <69er.11.11@gmail.com>
2023-04-08 17:28:14 +0900

    test: fix test_index

--- test_index.py
+++ test_index.py
@@ -1,18 +1,8 @@
-from index import sum
 from index import app
-import sys
-
-def test_sum(mocker):
-    mocker.patch.object(sys, 'argv', ['test_index.py', '2', '3'])
-    result = sum(int(sys.argv[1]), int(sys.argv[2]))
-    assert result == 5
-
-    result = sum(2,3)
-    assert result == 5
 
 def test_index():
     with app.test_client() as client:
-        response = client.get('/')
+        response = client.get('/index')
         assert response.status_code == 200
         assert b'<!DOCTYPE html>' in response.data
 
