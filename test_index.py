05870f4480d266419b5940fd8575c9c65c32598f
nanami69 <69er.11.11@gmail.com>
2023-04-09 16:03:24 +0900

    test: add test_generate_summary()

--- test_index.py
+++ test_index.py
@@ -1,4 +1,4 @@
-from index import app
+from index import app, generate_summary
 
 def test_index():
     with app.test_client() as client:
@@ -11,3 +11,9 @@ def test_sub():
         response = client.get('/sub')
         assert response.status_code == 200
         assert b'This is Sub Page!' in response.data
+
+def test_generate_summary():
+    prompt = "ニューヨークで人気のある観光スポット"
+    summary = generate_summary(prompt)
+    assert isinstance(summary, str)
+    assert len(summary) > 0
